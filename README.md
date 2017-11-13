6a84956900638f7c049687f9bd35ae8d74d96707
Nitin Sharma <nsharma78@gmail.com>
2017-11-12 18:20:03 -0800

    11/12/2017

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Tries
+
+1) Implement Trie (Prefix Tree)
+2) Add and Search Word - Data structure design (DFS)
---
+++ src/main/java/com/nitin/algo/AddSearchWordTrie.java
@@ -0,0 +1,15 @@
+/*
+ * Design a data structure that supports the following two operations:
+ * void addWord(word)
+ * bool search(word)
+ * search(word) can search a literal word or a regular expression string containing only letters a-z or .. A . means it can represent any one letter.
+ */
+package com.nitin.algo;
+
+public class AddSearchWordTrie {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
