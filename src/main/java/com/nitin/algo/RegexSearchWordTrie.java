34f5b71d792a44b68ae346f16309442ea895e589
Nitin Sharma <nsharma78@gmail.com>
2017-12-11 15:15:39 -0800

    RegexSearchWordTrie implemented

--- src/main/java/com/nitin/algo/RegexSearchWordTrie.java
+++ src/main/java/com/nitin/algo/RegexSearchWordTrie.java
@@ -4,11 +4,94 @@
  * bool search(word)
  * search(word) can search a literal word or a regular expression string containing only letters a-z or .. A . means it can represent any one letter.
  */
+
 package com.nitin.algo;
 
 public class RegexSearchWordTrie {
 	
+	private static TrieNode root;
+	public RegexSearchWordTrie() {
+		root = new TrieNode();
+	}
+	
+	static class TrieNode{
+		TrieNode[] children;
+		boolean isLeaf;
+		public TrieNode() {
+			children = new TrieNode[26];
+		}
+	}
+	
+	private static void addWord(String word) {
+		TrieNode node = root;
+		for(int i = 0; i < word.length(); i++) {
+			char c = word.charAt(i);
+			int index = c - 'a';
+			if(node.children[index] == null) {
+				TrieNode temp = new TrieNode();
+				node.children[index] = temp;
+				node = temp;
+			}else {
+				node = node.children[index];
+			}
+		}
+		node.isLeaf = true;
+		
+	}
+	
+	private static boolean search(String word) {
+		return dfsSearch(root, word, 0);
+	}
+	
+	private static boolean dfsSearch(TrieNode node, String word, int start) {
+		if (node.isLeaf && start == word.length())
+			return true;
+		if(start >= word.length())
+			return false;
+		
+		char c = word.charAt(start);
+		
+		if(c == '.') {
+			boolean tResult = false;
+			for (int j = 0; j < 26; j++) {
+				if (node.children[j] != null) {
+					if (dfsSearch(node.children[j], word, start + 1)) {
+						tResult = true;
+						break;
+					}
+				}
+			}
+ 
+			if (tResult)
+				return true;
+		}else {
+			int index = c - 'a';
+			 
+			if (node.children[index] != null) {
+				return dfsSearch(node.children[index], word, start + 1);
+			} else {
+				return false;
+			}
+		}
+		
+		return false;
+	}
+
 	public static void main(String[] args) {
+		new RegexSearchWordTrie();
+		String s1 = "care";
+		String s2 = "cart";
+		String s3 = "cape";
+		
+		addWord(s1);
+		addWord(s2);
+		addWord(s3);
+		
+		String s4 = "c.re";
+		System.out.println(s4 + " is present in the trie: " + search(s4) + "\n");
+		
+		String s5 = "c.rd";
+		System.out.println(s5 + " is present in the trie: " + search(s5) + "\n");
 		
 	}
 
---
+++ src/main/java/com/nitin/algo/Trie.java
@@ -0,0 +1,89 @@
+/*
+ * Implement a trie with insert, search, and startsWith methods using Map to store children
+ * A trie node should contains the character, its children and the flag that marks if it is a leaf node. You can use this diagram to walk though the Java solution.
+ */
+
+package com.nitin.algo;
+
+public class Trie {
+	private static TrieNode root;
+	
+	public Trie(){
+		root = new TrieNode();
+	}
+	
+	static class TrieNode{
+		TrieNode[] children;
+		boolean isLeaf;
+		
+		public TrieNode() {
+			children = new TrieNode[26];
+		}
+	}
+	
+	private static boolean startsWith(String prefix) {
+		TrieNode node = searchWord(prefix);
+		return (node != null) ? true : false;
+	}
+
+	private static boolean search(String word) {
+		TrieNode node = searchWord(word);
+		return (node != null && node.isLeaf) ? true : false;
+	}
+	
+	private static TrieNode searchWord(String word) {
+		TrieNode node = root;
+		for(int i = 0; i < word.length(); i++) {
+			char c = word.charAt(i);
+			int index = c - 'a';
+			if(node.children[index] == null) {
+				return null;
+			}else {
+				node = node.children[index];
+			}
+		}
+		return (node == root) ? null : node;
+	}
+
+	private static void insert(String word) {
+		TrieNode node = root;
+		for(int i = 0; i < word.length(); i++) {
+			char c = word.charAt(i);
+			int index = c - 'a';
+			if(node.children[index] == null) {
+				TrieNode temp = new TrieNode();
+				node.children[index] = temp;
+				node = temp;
+			}else {
+				node = node.children[index];
+			}
+		}
+		node.isLeaf = true;
+	}
+	
+	public static void main(String[] args) {
+		new Trie();
+		String s1 = "care";
+		String s2 = "cart";
+		String s3 = "cape";
+		
+		insert(s1);
+		insert(s2);
+		insert(s3);
+		
+		String s4 = s1;
+		System.out.println(s4 + " is present in the trie: " + search(s4) + "\n");
+		
+		String s5 = "cal";
+		System.out.println(s5 + " is present in the trie: " + search(s5) + "\n");
+		
+		String s6 = "ca";
+		System.out.println("The trie contains words with the prefix " + s6 + ": "+ startsWith(s6) + "\n");
+		
+		String s7 = "aa";
+		System.out.println("The trie contains words with the prefix " + s7 + ": "+ startsWith(s7) + "\n");
+		
+		
+	}
+
+}
