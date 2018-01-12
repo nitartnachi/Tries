50f75d9fb6323a852956bf4731113df1ad0e0295
Nitin Sharma <nsharma78@gmail.com>
2018-01-11 17:34:13 -0800

    WordBreak implemented

--- src/main/java/com/nitin/algo/WordBreak.java
+++ src/main/java/com/nitin/algo/WordBreak.java
@@ -19,6 +19,64 @@ The string can be segmented as "i like samsung" or
 
 package com.nitin.algo;
 
+import com.nitin.algo.Trie.TrieNode;
+
 public class WordBreak {
+	
+	private static TrieNode root;
+	
+	public WordBreak() {
+		root = new TrieNode();
+	}
+	
+	private static void insert(String word) {
+		TrieNode node = root;
+		for(int i = 0; i < word.length(); i++) {
+			char c = word.charAt(i);
+			int index = c - 'a';
+			if(node.children[index] == null) {
+				TrieNode temp = new TrieNode();
+				node.children[index] = temp;
+				node = temp;
+			}else {
+				node  = node.children[index];
+			}
+		}
+		node.isLeaf = true;
+	}
+	
+	private static boolean searchWord(String word) {
+		TrieNode node = root;
+		for(int i = 0; i < word.length(); i++) {
+			char c = word.charAt(i);
+			int index = c - 'a';
+			if(node.children[index] == null) {
+				return false;
+			}else {
+				node = node.children[index];
+			}
+		}
+		return (node == root) ? false : true;
+	}
+	
+	private static boolean wordBreak(String input) {
+		int size = input.length();
+		if(size == 0)
+			return true;
+		for(int i = 0; i <= size; i++) {
+			if(searchWord(input.substring(0, i)) && wordBreak(input.substring(i, size)))
+				return true;
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		new WordBreak();
+		String input = "iceacream";
+		String[] arr = {"i", "like", "sam", "sung", "samsung", "mobile", "ice", "cream", "icecream", "man", "go", "mango"};
+		for(String word : arr)
+			insert(word);
+		System.out.println("Possible: " + wordBreak(input));	
+	}
 
 }
